7a2af0c022066eb3abcb12736d490c88983fa2a0
Kogane-Yakumo <51345842+Kogane-Yakumo@users.noreply.github.com>
2023-09-02 18:34:49 -0700

    Functional

--- js.js
+++ js.js
@@ -1,21 +1,37 @@
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }  
+
 $("#activate").click(function () {
-    $.ajax({
-        url: "https://api.scryfall.com/cards/search?q=usd%3C0.06",
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            $("#output").html("")
-            var currData = data[0];
-            $("#output").append("<p> For " + currData.value + " points.");
-            $("#output").append("<p> In the catagory: " + currData.category.title);
-            $("#output").append("<p>" + currData.question);
-            $("#output").append("<p> What is " + currData.answer);
-            console.log(JSON.stringify(data, null, 2));
-        },
-        error: function (jqXHR, textStatus, errorThrown) { 
-            $("#output").html("")
-            $("#output").append("An execption has occurred.")
-            console.log("Error:", textStatus, errorThrown);
-        }
-    })
+    $("#output").html("Rotated Cards:")
+    var deckList = $("#decklist").val();
+    var deckList = deckList.split("\n")
+    var deckCards = deckList.flatMap((x) => x.slice(2));
+    var rotations = 0;
+    for (let i = 0; i < deckCards.length; i++) {
+        $.ajax({
+            url: "https://api.scryfall.com/cards/search?q=usd<0.06+" + deckCards[i].replaceAll(" ","-"),
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+                console.log(JSON.stringify(data, null, 2));
+                
+            },
+            error: function (jqXHR, textStatus, errorThrown) { 
+                $("#output").append("<p>" + deckCards[i]);
+                rotations += 1;
+                console.log("Error:", textStatus, errorThrown);
+                // It kinda sucks that we're litterally using 404 errors to determine rotations but this is the simplest way to do this.
+            }
+        })
+        sleep(70);
+        // This is here so that we don't get yelled at by Scryfall for flooding their API with requests
+    }
+    if (rotations == 0){
+        $("#output").html("You're all good!")
+    }
 })
